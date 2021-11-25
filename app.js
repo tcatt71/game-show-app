0949d0a8990c4910b7fadf086eeb5eef54595f83
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-11-25 01:56:51 -0800

    Add start game overlay removal functionality

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const qwerty = document.querySelector('#qwerty');
   const phrase = document.querySelector('#phrase');
-  const gameStartBtn = document.querySelector('btn__reset');
+  const gameStartBtn = document.querySelector('.btn__reset');
   let missed = 0;
+
+  gameStartBtn.addEventListener('click', () => {
+    const overlay = document.querySelector('#overlay');
+    overlay.style.display = 'none';
+  });
 });
