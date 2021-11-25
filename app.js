7bf3a2e71206ce9726e58a54b36af0a7a9b9477c
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-11-25 01:31:38 -0800

    Add variables

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const qwerty = document.querySelector('#qwerty');
+  const phrase = document.querySelector('#phrase');
+  const gameStartBtn = document.querySelector('btn__reset');
+  let missed = 0;
+});
